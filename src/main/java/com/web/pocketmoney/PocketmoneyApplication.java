ccb1722e92f3ef2a14b17e50e612301f30958b52
yejin <yaezzin@gmail.com>
2022-06-25 15:45:51 +0900

    initial commit

---
+++ src/main/java/com/web/pocketmoney/PocketmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.web.pocketmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocketmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocketmoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/pocketmoney/PocketmoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.pocketmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocketmoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
