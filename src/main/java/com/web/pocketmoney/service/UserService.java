4efc9ffdaa05677fc7ee1c33979b8d33aa5a4746
gomcool17 <gomcool17@naver.com>
2022-06-27 15:36:19 +0900

    [UPDATE] 아이디, 닉네임 중복 체크

--- src/main/java/com/web/pocketmoney/service/UserService.java
+++ src/main/java/com/web/pocketmoney/service/UserService.java
@@ -13,15 +13,16 @@ public class UserService {
     private UserRepository userRepository;
 
     public User save(User user) {
-       // validateDuplicateUserEmail(user);
-       // validateDuplicateUserNickName(user);
+        validateDuplicateUserEmail(user);
+        validateDuplicateUserNickName(user);
         userRepository.save(user);
         return user;
     }
 
     private void validateDuplicateUserEmail(User user) {
         List<User> findEmails = userRepository.findByEmail(user.getEmail());
-        if(!findEmails.isEmpty()){
+        if(!findEmails.isEmpty()) {
+            System.out.println(user.getEmail() + " : ");
             throw new IllegalStateException("이미 존재하는 아이디 입니다.");
         }
     }
@@ -29,6 +30,7 @@ public class UserService {
     private void validateDuplicateUserNickName(User user) {
         List<User> findNickNames = userRepository.findByNickName(user.getNickName());
         if(!findNickNames.isEmpty()){
+            System.out.println(user.getNickName() + " : ");
             throw new IllegalStateException("이미 존재하는 닉네임 입니다.");
         }
     }
