60a76092e824cfeb48ba59fba971fccb1e46928e
username <email@email.com>
2022-07-08 04:17:54 +0900

    [ADD] : MessageController 추가

---
+++ src/main/java/com/web/pocketmoney/controller/message/MessageController.java
@@ -0,0 +1,20 @@
+package com.web.pocketmoney.controller.message;
+
+import com.web.pocketmoney.service.message.MessageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MessageController {
+    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final MessageService messageService;
+    @MessageMapping("/chat/send")
+    public void sendMessage(MessageForm message) {
+        String receiver = message.getReceiver();
+        messageService.save(message);
+        simpMessagingTemplate.convertAndSend("/topic/"+ receiver,message);
+    }
+}
