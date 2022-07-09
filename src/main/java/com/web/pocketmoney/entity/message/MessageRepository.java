47fef2eeb6f17681bee3a76c43a94b5cb351732c
username <email@email.com>
2022-07-10 02:03:58 +0900

    [UPDATE] Repository 수정

--- src/main/java/com/web/pocketmoney/entity/message/MessageRepository.java
+++ src/main/java/com/web/pocketmoney/entity/message/MessageRepository.java
@@ -1,11 +1,18 @@
 package com.web.pocketmoney.entity.message;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+    long countBySenderIdAndRecipientIdAndStatus(Long senderId, Long recipientId, MessageStatus status);
+
+    List<Message> findByChatId(String chatId);
+
+    @Modifying
+    @Query("UPDATE Message mg set mg.status = :status WHERE mg.senderId = :senderId and mg.recipientId = :recipientId ")
+    Message updateStatus(Long senderId, Long recipientId, MessageStatus status);
 }
