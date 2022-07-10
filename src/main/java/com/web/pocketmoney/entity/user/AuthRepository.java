0d28d1e763de996fa716d717690c2135a617460f
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-07-10 18:55:40 +0900

    [ADD] react 시도

--- src/main/java/com/web/pocketmoney/entity/user/AuthRepository.java
+++ src/main/java/com/web/pocketmoney/entity/user/AuthRepository.java
@@ -1,2 +1,19 @@
-package com.web.pocketmoney.entity.user;public class AuthRepository {
+package com.web.pocketmoney.entity.user;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuthRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+    User findByUserId(String userId);
+
+    boolean existsByNickname(String nickname);
+
+    boolean existsByEmail(String email);
+
+
 }
