780b6c2e26e29ea0d271ea239190b145f1425702
yejin <yaezzin@gmail.com>
2022-07-10 17:48:59 +0900

    [ADD] 게시판 페이징 처리

--- src/main/java/com/web/pocketmoney/controller/board/BoardIndexController.java
+++ src/main/java/com/web/pocketmoney/controller/board/BoardIndexController.java
@@ -1,9 +1,14 @@
 package com.web.pocketmoney.controller.board;
 
 import com.web.pocketmoney.dto.board.BoardResponseDto;
+import com.web.pocketmoney.entity.board.Board;
 import com.web.pocketmoney.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,9 +21,17 @@ public class BoardIndexController {
 
     private final BoardService boardService;
 
+    /* 글 전체 목록 조회 */
     @GetMapping("/")
-    public String index(Model model) {
-        return "index";
+    public String index(Model model, @PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+            Page<Board> list = boardService.pageList(pageable);
+
+            model.addAttribute("boards", boardService.pageList(pageable));
+            model.addAttribute("previous", pageable.previousOrFirst().getPageNumber()); //이전 페이지
+            model.addAttribute("next", pageable.next().getPageNumber()); // 다음 페이지
+            model.addAttribute("hasNext", list.hasNext()); // 이전, 다음 페이지 유무에 따른 T, F 반환
+            model.addAttribute("hasPrev", list.hasPrevious());
+            return "boards";
     }
 
     /* 글 작성 */
@@ -43,6 +56,5 @@ public class BoardIndexController {
         model.addAttribute("board", dto);
         return "board/update";
     }
-
 }
 
