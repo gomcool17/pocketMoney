303b364ace482c53d53ac97090b05a899bc8737c
yejin <yaezzin@gmail.com>
2022-07-10 17:30:07 +0900

    [ADD] Board 등록, 조회, 수정, 삭제 API 추가

---
+++ src/main/java/com/web/pocketmoney/controller/board/BoardIndexController.java
@@ -0,0 +1,47 @@
+package com.web.pocketmoney.controller.board;
+
+import com.web.pocketmoney.dto.board.BoardResponseDto;
+import com.web.pocketmoney.service.board.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class BoardIndexController {
+
+    private final BoardService boardService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    /* 글 작성 */
+    @GetMapping("/board/write")
+    public String write(Model model) {
+        return "board/write";
+    }
+
+    /* 글 상세 보기 */
+    @GetMapping("/board/read/{id}")
+    public String read(@PathVariable Long id, Model model) {
+        BoardResponseDto dto = boardService.findById(id);
+        model.addAttribute("board", dto);
+        return "board/read";
+    }
+
+    /* 글 수정 */
+    @GetMapping("/board/update/{id}")
+    public String update(@PathVariable Long id, Model model) {
+        BoardResponseDto dto = boardService.findById(id);
+        model.addAttribute("board", dto);
+        return "board/update";
+    }
+
+}
+
---
+++ src/main/java/com/web/pocketmoney/service/board/BoardService.java
@@ -0,0 +1,44 @@
+package com.web.pocketmoney.service.board;
+
+import com.web.pocketmoney.dto.board.BoardResponseDto;
+import com.web.pocketmoney.dto.board.BoardSaveRequestDto;
+import com.web.pocketmoney.dto.board.BoardUpdateRequestDto;
+import com.web.pocketmoney.entity.board.Board;
+import com.web.pocketmoney.entity.board.BoardRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public BoardResponseDto save(BoardSaveRequestDto requestDto) {
+        return new BoardResponseDto(boardRepository.save(requestDto.toEntity()));
+    }
+
+    @Transactional
+    public BoardResponseDto findById(Long id) {
+        Board board = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException());
+        return new BoardResponseDto(board);
+    }
+
+    @Transactional
+    public BoardResponseDto update(Long id, BoardUpdateRequestDto requestDto) {
+        Board findBoard = boardRepository.findById(id).orElseThrow();
+        findBoard.update(requestDto.getTitle(), requestDto.getContent(), requestDto.getArea(), requestDto.getWantedTime(),
+                requestDto.getDayOfWeek(), requestDto.getPay(), requestDto.getViewCount(), requestDto.getLikes());
+        return new BoardResponseDto(findBoard);
+    }
+
+    public void delete(Long id) {
+        Board findBoard = boardRepository.findById(id).orElseThrow();
+        boardRepository.delete(findBoard);
+    }
+}
