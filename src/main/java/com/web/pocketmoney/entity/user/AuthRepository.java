c7d08a2af348190d1cfb14b2682188d3e0e75325
SimDaeSung <86938974+ShimDaeSung1@users.noreply.github.com>
2022-07-12 21:16:31 +0900

    [UPDATE] User 정보 조회/수정/삭제

--- src/main/java/com/web/pocketmoney/entity/user/AuthRepository.java
+++ src/main/java/com/web/pocketmoney/entity/user/AuthRepository.java
@@ -1,19 +1,19 @@
-package com.web.pocketmoney.entity.user;
-
-import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AuthRepository extends JpaRepository<User, Long> {
-
-    User findByEmail(String email);
-
-    User findByUserId(String userId);
-
-    boolean existsByNickName(String nickname);
-
-    boolean existsByEmail(String email);
-
-
-}
+//package com.web.pocketmoney.entity.user;
+//
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface AuthRepository extends JpaRepository<User, Long> {
+//
+//    User findByEmail(String email);
+//
+//    User findByUserId(String userId);
+//
+//    boolean existsByNickName(String nickname);
+//
+//    boolean existsByEmail(String email);
+//
+//
+//}
