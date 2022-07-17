704f28ee17b48a00c23a69db722df936eb856ac6
gomcool17 <gomcool17@naver.com>
2022-07-17 17:22:27 +0900

    []

--- src/main/java/com/web/pocketmoney/service/UserServiceImpl.java
+++ src/main/java/com/web/pocketmoney/service/UserServiceImpl.java
@@ -50,9 +50,11 @@ public class UserServiceImpl implements UserService{
         // 수정시에는 영속성 컨텍스트 User 오브젝트를 영속화시키고, 영속화된 User 오브젝트를 수정
         // select를 해서 User오브젝트를 db로 부터 가져오는 이유는 영속화를 하기 위해서
         // 영속화된 오브젝트를 변경하면 DB에 Update문을 날려주기 때문
+        log.info(user.toString());
         User persistance = userRepository.findById(user.getId()).orElseThrow(()->{
             return new IllegalArgumentException("회원 찾기 실패");
         });
+        log.info(persistance.toString());
         //oauth에 값이 없으면 수정 가능
         //Validate 체크,OAuth로그인한 사람들은 비밀번호 변경 불가
         if(persistance.getOauth()==null || persistance.getOauth().equals("")){
