0def90bc469d879443712e85b9fd4ec8ca114b9f
khdscor <khds_cor@naver.com>
2022-07-31 17:30:35 +0900

    [ADD] 회원가입 요구 변수 증가

--- frontend/src/component/login/LoginPage.js
+++ frontend/src/component/login/LoginPage.js
@@ -1,32 +1,54 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import EmailLogin from './EmailLogin';
-import {HomeButton, LoginButton, LoginPageBox, StyledDiv} from "./Box";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import EmailLogin from "./EmailLogin";
+import { HomeButton, LoginButton, LoginPageBox, StyledDiv } from "./Box";
 
 function LoginPage() {
-    const navigate = useNavigate();
-    return (<>
-        <HomeButton onClick={()=>{navigate("/")}}/>
-          <LoginPageBox>
-            <EmailLogin />
-            <LoginButton style={{
-                  color: "yellow",
-                  borderColor: "yellow"
-                }}
-            >카카오로그인</LoginButton >
-            <LoginButton style={{
-                  color: "green",
-                  borderColor: "green"
-                }}
-            >네이버로그인</LoginButton>
-            <LoginButton style={{
-                  color: "gray",
-                  borderColor: "gray"
-                }}
-            >구글로그인</LoginButton>
-            <StyledDiv onClick={()=>{navigate("/signup")}}>PocketMoney 회원가입</StyledDiv>
-          </LoginPageBox>
-      </>);
+  const navigate = useNavigate();
+  return (
+    <>
+      <HomeButton
+        onClick={() => {
+          navigate("/");
+        }}
+      />
+      <LoginPageBox>
+        <EmailLogin />
+        <LoginButton
+          style={{
+            color: "yellow",
+            borderColor: "yellow",
+          }}
+        >
+          카카오로그인
+        </LoginButton>
+        <LoginButton
+          style={{
+            color: "green",
+            borderColor: "green",
+          }}
+        >
+          네이버로그인
+        </LoginButton>
+        <LoginButton
+          sty
+          le={{
+            color: "gray",
+            borderColor: "gray",
+          }}
+        >
+          구글로그인
+        </LoginButton>
+        <StyledDiv
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          PocketMoney 회원가입
+        </StyledDiv>
+      </LoginPageBox>
+    </>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- frontend/src/component/login/SignUpPage.js
+++ frontend/src/component/login/SignUpPage.js
@@ -1,55 +1,94 @@
-import React, {useState} from 'react';
-import {HomeButton, LoginButton, StyledInput, SignUpPageBox} from "./Box";
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { HomeButton, LoginButton, StyledInput, SignUpPageBox } from "./Box";
+import { useNavigate } from "react-router-dom";
 
 const SignUpPage = (props) => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
   const [nickName, setNickName] = useState("");
+  const [age, setAge] = useState("");
+  const [sex, setSex] = useState("");
+  const [city, setCity] = useState("");
+  const [userName, setUserName] = useState("");
 
-  return (<>
-      <HomeButton onClick={()=>{navigate("/")}}/>
+  return (
+    <>
+      <HomeButton
+        onClick={() => {
+          navigate("/");
+        }}
+      />
       <SignUpPageBox>
         <StyledInput
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-            placeholder={"이메일"}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder={"이메일"}
         />
         <StyledInput
-            type='password'
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-            placeholder={"비밀번호"}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder={"비밀번호"}
         />
         <StyledInput
-            type='password'
-            value={checkPassword}
-            onChange={e => setCheckPassword(e.target.value)}
-            placeholder={"비밀번호 재확인"}
+          type="password"
+          value={checkPassword}
+          onChange={(e) => setCheckPassword(e.target.value)}
+          placeholder={"비밀번호 재확인"}
         />
         <StyledInput
-            value={nickName}
-            onChange={e => setNickName(e.target.value)}
-            placeholder={"이름"}
+          value={nickName}
+          onChange={(e) => setNickName(e.target.value)}
+          placeholder={"닉네임"}
+        />
+        <StyledInput
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          placeholder={"이름"}
+        />
+        <StyledInput
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+          placeholder={"나이"}
+        />
+        <StyledInput
+          value={sex}
+          onChange={(e) => setSex(e.target.value)}
+          placeholder={"성별"}
+        />
+        <StyledInput
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          placeholder={"도시"}
         />
         <LoginButton
-            onClick={() => {
-              if(email.length && password.length && checkPassword.length && nickName.length){
-                if(password === checkPassword){
-                  //회원가입 진행
-                }
-                else
-                  alert("비밀번호가 일치하지 않습니다");
-              }
-              else{
-                alert("빈칸을 다 채워주세요");
-              }
-              }}>가입하기</LoginButton>
+          onClick={() => {
+            if (
+              email.length &&
+              password.length &&
+              checkPassword.length &&
+              nickName.length &&
+              age.length &&
+              sex.length &&
+              city.length &&
+              userName.length
+            ) {
+              if (password === checkPassword) {
+                //회원가입 진행
+              } else alert("비밀번호가 일치하지 않습니다");
+            } else {
+              alert("빈칸을 다 채워주세요");
+            }
+          }}
+        >
+          가입하기
+        </LoginButton>
       </SignUpPageBox>
-  </>);
+    </>
+  );
 };
 
 export default SignUpPage;
