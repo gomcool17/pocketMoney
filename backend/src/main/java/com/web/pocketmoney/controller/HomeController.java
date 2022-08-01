75072d085fdb7092fe20472c193068c484012fd4
gomcool17 <gomcool17@naver.com>
2022-08-01 20:03:37 +0900

    [UPDATE] kakao token api방신 변경

--- backend/src/main/java/com/web/pocketmoney/controller/HomeController.java
+++ backend/src/main/java/com/web/pocketmoney/controller/HomeController.java
@@ -2,12 +2,13 @@ package com.web.pocketmoney.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @GetMapping("/home")
-    public String home() {
-        return "index";
-    }
+   // @GetMapping("/")
+   // public String home() {
+      //  return "index";
+   // }
 }
