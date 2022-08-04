08ccfa0e4e0491f00740254812a10ac3e99eb23c
khdscor <khds_cor@naver.com>
2022-08-04 22:22:16 +0900

    [FIX] 폴더 정리 및 마이페이지 틀 수정

--- frontend/src/component/mypage/MyPage.js
+++ frontend/src/component/mypage/MyPage.js
@@ -4,6 +4,7 @@ import { ACCESS_TOKEN } from "./../../constant/LocalStorage";
 import styled from "styled-components";
 import { useNavigate } from "react-router";
 import CancelButton from "./../CancelButton";
+import InfBox from "./InfBox";
 
 const Outside = styled.div`
   width: 1050px;
@@ -19,18 +20,6 @@ const BaseImg = styled.div`
   width: 300px;
   height: 300px;
 `;
-const InfBox = styled.div`
-  display: inline-block;
-  width: 300px;
-  height: 300px;
-  padding-left: 100px;
-`;
-const NickName = styled.div`
-  width: 200px;
-  height: 30px;
-  margin: 10px;
-  border: 5px solid blue;
-`;
 const ChatButtonBox = styled.div`
   position: relative;
   display: inline-block;
@@ -41,10 +30,11 @@ const ChatButton = styled.div`
   position: absolute;
   top: 120px;
   left: 110px;
-  width: 100px;
-  height: 40px;
-  font-size: 30px;
+  width: 130px;
+  height: 50px;
+  font-size: 40px;
   background-color: pink;
+  border-radius: 100px;
   cursor: pointer;
 `;
 
@@ -71,14 +61,7 @@ function Mypage() {
             }}
           />
         </BaseImg>
-        <InfBox>
-          <NickName></NickName>
-          <NickName></NickName>
-          <NickName></NickName>
-          <NickName></NickName>
-          <NickName></NickName>
-          <NickName></NickName>
-        </InfBox>
+        <InfBox />
         <ChatButtonBox>
           <ChatButton
             onClick={() => {
