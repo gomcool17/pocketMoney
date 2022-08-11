82ac28712c5287fc6bd760c0e1598c037b30428e
gomcool17 <gomcool17@naver.com>
2022-08-11 20:37:23 +0900

    [UPDATE] 게시글 들어갈시 조회수 증가, 어떤 사용자인지

---
+++ backend/src/main/java/com/web/pocketmoney/dto/board/BoardDto.java
@@ -0,0 +1,22 @@
+package com.web.pocketmoney.dto.board;
+
+import com.web.pocketmoney.entity.board.Board;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+public class BoardDto {
+    private String title;
+    private String content;
+    private String area;
+    private List<Integer> dayOfWeek;
+    private LocalDateTime date;
+    private int pay;
+    private int view;
+    private String nickName;
+    private int isUser; //
+}
